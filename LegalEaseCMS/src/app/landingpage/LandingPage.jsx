d368e3e695602af945bb08b136ec78e49154ddc8
themaryanjuguna <themaryanjuguna@gmail.com>
2024-04-20 02:18:03 +0300

    component + navbar created

---
+++ LegalEaseCMS/src/app/landingpage/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hero from '../../components/landingpage/Hero'
+
+function LandingPage() {
+  return (
+    <Hero />
+  )
+}
+
+export default LandingPage
---
+++ LegalEaseCMS/src/components/landingpage/Hero.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900">
+    <div className="py-8 px-12 mx-auto max-w-screen-xl lg:py-16 flex flex-col lg:flex-row justify-between gap-8 lg:gap-16">
+        <div className="flex flex-col justify-center lg:w-1/2">
+            <h3 className="mb-4 text-2xl font-semibold leading-none text-gray-900 md:text-2xl lg:text-2xl dark:text-white">LegalEase CMS</h3>
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Revolutionizing Legal Workflow</h1>
+            <h3 className="mb-4 text-2xl font-regular leading-none text-gray-900 md:text-2xl lg:text-2xl dark:text-white">Unlock the Power of Simplified Legal Management</h3>
+            <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+            Navigate the complexities of legal documentation, client communications, and case file integrations with ease. LegalEase CMS is tailored to meet the unique demands of legal professionals, making your daily operations smoother and more efficient.
+            </p>
+            <a href="#" className="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read more about our app 
+                <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+        </div>
+        <div className="lg:w-1/2">
+            <div className="w-full p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
+                <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
+                    Sign in to LegalLease
+                </h2>
+                <form className="mt-8 space-y-6" action="#">
+                    <div>
+                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                    </div>
+                    <div>
+                        <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    </div>
+                    <div className="flex items-start">
+                        <div className="flex items-center h-5">
+                            <input id="remember" aria-describedby="remember" name="remember" type="checkbox" className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600" required />
+                        </div>
+                        <div className="ms-3 text-sm">
+                        <label for="remember" className="font-medium text-gray-500 dark:text-gray-400">Remember this device</label>
+                        </div>
+                        <a href="#" className="ms-auto text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Lost Password?</a>
+                    </div>
+                    <button type="submit" className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+                    <div className="text-sm font-medium text-gray-900 dark:text-white">
+                        Not registered yet? <a className="text-blue-600 hover:underline dark:text-blue-500">Create account</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+  )
+}
+
+export default Hero
