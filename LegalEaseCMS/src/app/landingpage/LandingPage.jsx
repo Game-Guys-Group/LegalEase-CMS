cc2bcee77d1698bd79905d9a16185386751a3fb4
themaryanjuguna <themaryanjuguna@gmail.com>
2024-04-24 15:22:48 +0300

    Routes update

--- LegalEaseCMS/src/app/landingpage/LandingPage.jsx
+++ LegalEaseCMS/src/app/landingpage/LandingPage.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Hero from '../../components/pages/landingpage/Hero'
 import WhyUs from '../../components/pages/landingpage/WhyUs'
+import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
+import Features from '../../components/pages/landingpage/Features'
+import Register from '../../components/pages/landingpage/Register'
 
 function LandingPage() {
   return (
     <>
+    <Navbar />
     <Hero />
     <WhyUs />
+    <Features />
+    <Register />
     <Footer />
     </>
   )
