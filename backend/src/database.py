b2e5f0615c982937b43f40fa8edfefd75130c536
Travor Oguna <trevoroguna@gmail.com>
2024-04-28 16:43:32 +0300

    backend: remove connect args in release mode

--- backend/src/database.py
+++ backend/src/database.py
@@ -3,10 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .settings import settings
 
-
-engine = create_engine(
-    settings.sqlalchemy_database_url, connect_args={"check_same_thread": False}
-)
+if settings.mode == "dev":
+    engine = create_engine(
+        settings.sqlalchemy_database_url, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(settings.sqlalchemy_database_url )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
